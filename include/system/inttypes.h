d246d5ee3988c3d4e6c885eb505c26f6490c1cdd
Peter Jones <pjones@redhat.com>
2022-05-26 15:56:34 -0400

    inttypes.h: add a few of the very common PRIfNN defines
    This adds the PRI[dux]32 and PRI[dux]64 format definitions.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- include/system/inttypes.h
+++ include/system/inttypes.h
@@ -8,6 +8,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#if __SIZEOF_LONG__ == 8
+#define PRIu64	"lu"
+#define PRIx64	"lx"
+#define PRId64	"ld"
+#else
+#define PRIu64	"llu"
+#define PRIx64	"llx"
+#define PRId64	"lld"
+#endif
+#define PRIu32	"u"
+#define PRIx32	"x"
+#define PRId32	"d"
+
 #endif /* !INTTYPES_H_ */
 #endif
 // vim:fenc=utf-8:tw=75:noet
