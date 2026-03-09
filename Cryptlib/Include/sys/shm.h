c49add05ed85771126dd0501674e910857652d07
Nicolas Frayer <nfrayer@redhat.com>
2026-03-09 15:18:27 +0100

    Include files part 3: Merge for OpenSSL 3.5.0
    Signed-off-by: Nicolas Frayer <nfrayer@redhat.com>

--- Cryptlib/Include/sys/shm.h
+++ Cryptlib/Include/sys/shm.h
@@ -1,7 +1,7 @@
 /** @file
   Include file to support building the third-party cryptographic library.
 
-Copyright (c) 2010 - 2017, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
--- Cryptlib/Include/sys/syscall.h
+++ Cryptlib/Include/sys/syscall.h
@@ -2,6 +2,7 @@
   Include file to support building the third-party cryptographic library.
 
 Copyright (c) 2010 - 2017, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2019, Red Hat, Inc.
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
